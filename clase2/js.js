fdc56bfe491bdb5e568341b790de690132e7aa75
Your Name <you@example.com>
2021-03-01 13:31:04 -0400

    inicio de clase numeor 2 y el final

---
+++ clase2/js.js
@@ -0,0 +1,58 @@
+/*  comentario */
+// otro tipo de comentario
+
+//number= todo tipo de numero
+//string= cadena de texto
+//boolean = verdadero(True) o falso(false)
+
+
+//tipos de datos - coleccion de datos
+    //array []
+    //objetos= colecciones de datos(informacion)tienen un identificador y un valor
+
+["jose", "josé", "juan", "juan", "robert"]
+
+    //console.log( ["jose", "josé", "juan", "juan", "robert"]  )
+
+//objeto
+    console.log ({
+    nombre:"Beto", 
+    apellido:"Quiroga"
+    })
+//NULL nulo
+console.log(typeof null)
+
+//undefined
+console.log(typeof undefined)
+
+//variables
+    /* 
+    var:
+    let: 
+    */
+//declaracion
+/* let alumno
+alumno = "circuito integrado" 
+console.log (alumno) */
+
+//ejemplo de declaracion y asignacion
+    //let profesor = "Luis Martines"
+   //console.log(profesor)
+
+//reasignacion 
+//profesor= "alvaro felipe"
+//console.log(profesor)
+//asignacion por referencia
+
+/* let profesor = "alvaro felipe"
+let alumno = profesor
+
+profesor = "carlos"
+
+console.log(profesor)
+console.log(alumno) */
+
+// Constante : datos que no pueden cambiar
+const nombre_empresa = "edteam"
+    
+
--- inicio.js
+++ inicio.js
@@ -1,2 +1,10 @@
 console.log("primera clase de JS")
 alert("otra alerta mas")
+// para un comentario en js pero de una linea
+/* este es para un 
+comentario mas largo
+o sea, varias lineas
+*/
+/* var hola= "saludo"
+console.log(hola)
+console.log("adios") */
